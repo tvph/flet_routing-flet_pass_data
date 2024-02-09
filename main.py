92389c6de17db69cb3329db42584782c275f533a
Tev <60355937+tvph@users.noreply.github.com>
2024-02-09 15:53:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-# import flet as ft
-# from router_tev import Router
-#
-#
-# def main(page: ft.Page):
-#     page.title = "Routes Example"
-#     # page.theme_mode = "dark"
-#
-#     myrouter = Router(page)
-#     page.on_route_change = myrouter.route_change
-#     page.on_view_pop = myrouter.view_pop
-#     page.go(page.route)
-#
-#
-# if __name__ == '__main__':
-#     # ft.app(target=main, assets_dir="assets")
-#     ft.app(target=main, view=ft.AppView.WEB_BROWSER)
-
 import flet as ft
 from views.routes import router
 from app_bar import NavBar
